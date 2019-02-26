8a83c223e8dfacb67fc1e020a1c5e6dea0a271ef
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-02-26 16:28:14 +0000

    Setup Sonarqube testing

---
+++ test/unit/sonarqube_code_coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Unit test code coverage for SonarQube to cover all modules.
+# This will run the Python code coverage module against all unit test modules.
+# This will show the amount of code that was tested and which lines of code
+#	that was skipped during the test.
+
+coverage erase
+
+echo ""
+echo "Running unit test modules in conjunction with coverage"
+coverage run -a --source=elastic_libs test/unit/elastic_libs/list_repos2.py
+coverage run -a --source=elastic_libs test/unit/elastic_libs/list_dumps.py
+coverage run -a --source=elastic_libs test/unit/elastic_libs/get_latest_dump.py
+
+echo ""
+echo "Producing code coverage report"
+coverage combine
+coverage report -m
+coverage xml -i
+
