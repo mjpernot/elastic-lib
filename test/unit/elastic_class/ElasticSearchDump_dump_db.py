81c8e26b14e28be2ff5db764cf282bc046b3372c
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-08-20 16:11:27 +0100

    Update test/unit/elastic_class/ElasticSearchDump_dump_db.py

--- test/unit/elastic_class/ElasticSearchDump_dump_db.py
+++ test/unit/elastic_class/ElasticSearchDump_dump_db.py
@@ -6,7 +6,7 @@
     Description:  Unit testing of dump_db in elastic_class.ElasticSearchDump.
 
     Usage:
-        test/unit/elastic_class/ElasticSearch_dump_db.py
+        test/unit/elastic_class/ElasticSearchDump_dump_db.py
 
     Arguments:
 
@@ -34,7 +34,51 @@ import version
 
 __version__ = version.__version__
 
-STOPPED HERE
+
+class Repo(object):
+
+    """Class:  Repo
+
+    Description:  Class representation of the snapshot class.
+
+    Super-Class:  object
+
+    Sub-Classes:
+
+    Methods:
+        get_repository -> Stub holder for snapshot.get_repository method.
+        create -> Stub holder for snapshot.create method.
+
+    """
+
+    def get_repository(self):
+
+        """Method:  get_repository
+
+        Description:  Stub holder for snapshot.get_repository method.
+
+        Arguments:
+
+        """
+
+        return {"reponame": {"type": "dbdump", "settings":
+                             {"location": "/dir/path/dump"}}}
+
+    def create(self, repository, body, snapshot):
+
+        """Method:  create
+
+        Description:  Stub holder for snapshot.create method.
+
+        Arguments:
+            (input) repository -> Repository name.
+            (input) body -> Database dump command.
+            (input) snapshot -> Database dump respository information.
+
+        """
+
+        return True
+
 
 class Elasticsearch(object):
 
@@ -67,6 +111,7 @@ class Elasticsearch(object):
         self.port = port
         self.ping_status = True
         self.info_status = {"cluster_name": "ClusterName", "name": "servername"}
+        self.snapshot = Repo()
 
     def ping(self):
 
@@ -105,7 +150,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialization for unit testing.
-        test_ping_false -> Test ping of Elasticsearch server is False.
+        test_ping_true -> Test ping of Elasticsearch server is True.
 
     """
 
@@ -121,24 +166,34 @@ class UnitTest(unittest.TestCase):
 
         self.host_list = ["host1", "host2"]
         self.host_str = "host1, host2"
+        self.repo = "reponame"
         self.es = Elasticsearch(self.host_list)
+        self.dbs = "dbname"
 
+    @mock.patch("elastic_class.ElasticSearchDump._chk_status")
+    @mock.patch("elastic_libs.get_latest_dump")
+    @mock.patch("elastic_class.get_dump_list")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_ping_false(self, mock_es):
+    def test_ping_true(self, mock_es, mock_list, mock_latest, mock_chk):
 
-        """Function:  test_ping_false
+        """Function:  test_ping_true
 
-        Description:  Test ping of Elasticsearch server is False.
+        Description:  Test ping of Elasticsearch server is True.
 
         Arguments:
 
         """
 
-        self.es.ping_status = False
         mock_es.return_value = self.es
+        mock_list.side_effect = [["dump1", "dump2"],
+                                 ["dump1", "dump2", "dump3"]]
+        mock_latest.side_effect = ["dump2", "dump3"]
+        mock_chk.return_value = (False, None, True)
 
-        es = elastic_class.ElasticSearch(self.host_list)
-        self.assertEqual((es.port, es.hosts), (9200, self.host_list))
+        es = elastic_class.ElasticSearchDump(self.host_list, repo=self.repo)
+        self.assertEqual(es.dump_db(self.dbs), (False, None))
+
+        #self.assertEqual((es.port, es.hosts), (9200, self.host_list))
 
 
 if __name__ == "__main__":
