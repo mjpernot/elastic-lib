ddf9dd7b8169261797d400057ab57b605f2cee9d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-08-21 15:42:24 +0100

    Update unit test runs

--- test/unit/elastic_class/unit_test_run.sh
+++ test/unit/elastic_class/unit_test_run.sh
@@ -9,5 +9,6 @@ echo "Unit test:  elastic_class"
 test/unit/elastic_class/ElasticSearch_init.py
 test/unit/elastic_class/ElasticSearchDump_init.py
 test/unit/elastic_class/ElasticSearchDump_dump_db.py
+test/unit/elastic_class/ElasticSearchRepo_create_repo.py
 test/unit/elastic_class/ElasticSearchRepo_delete_repo.py
 test/unit/elastic_class/ElasticSearchRepo_init.py
