d5cea2047d94badb053ba58d8cd758d261d75474
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-08-23 11:16:53 +0000

    Update ElasticSearchRepo_delete_dump_all.py

--- test/unit/elastic_class/ElasticSearchRepo_delete_dump_all.py
+++ test/unit/elastic_class/ElasticSearchRepo_delete_dump_all.py
@@ -42,10 +42,6 @@ class Repo(object):
 
     Description:  Class representation of the snapshot class.
 
-    Super-Class:  object
-
-    Sub-Classes:
-
     Methods:
         get_repository -> Stub holder for snapshot.get_repository method.
         create_repository -> Stub holder for snapshot.create_repository method.
@@ -96,10 +92,6 @@ class Elasticsearch(object):
     """Class:  ElasticSearch
 
     Description:  Class representation of the Elasticsearch class.
-
-    Super-Class:  object
-
-    Sub-Classes:
 
     Methods:
         __init__ -> Initialize configuration environment.
@@ -154,10 +146,6 @@ class UnitTest(unittest.TestCase):
     """Class:  UnitTest
 
     Description:  Class which is a representation of a unit testing.
-
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
 
     Methods:
         setUp -> Initialization for unit testing.
