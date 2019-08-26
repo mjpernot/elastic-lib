d5b13f5f94c8e93f2dd79a4dd0a7bf98fd6e9e92
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-08-26 08:49:09 +0000

    Update ElasticStatus_chk_nodes.py

--- test/unit/elastic_class/ElasticStatus_chk_nodes.py
+++ test/unit/elastic_class/ElasticStatus_chk_nodes.py
@@ -43,6 +43,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialization for unit testing.
+        test_string_default -> Test with string setting.
         test_default -> Test with default settings.
 
     """
@@ -89,6 +90,26 @@ class UnitTest(unittest.TestCase):
                                                    "free_in_bytes": 120000},
                                            "allocated_processors": 2}}}
         self.get_data9 = "995 69mb 16gb 53gb 69gb 23 ip1 ip2 hostname\n"
+
+    @mock.patch("elastic_class.requests_libs.get_query")
+    def test_string_default(self, mock_get):
+
+        """Function:  test_string_default
+
+        Description:  Test with string setting.
+
+        Arguments:
+
+        """
+
+        mock_get.side_effect = [self.get_data, self.get_data2, self.get_data3,
+                                self.get_data4, self.get_data5, self.get_data6,
+                                self.get_data7, self.get_data8, self.get_data9]
+
+        es = elastic_class.ElasticStatus(self.host_name)
+        es.failed_nodes = 1
+        self.assertEqual(es.chk_nodes(),
+            ("WARNING:  Have detected 1 failed nodes out of 1 nodes"))
 
     @mock.patch("elastic_class.requests_libs.get_query")
     def test_default(self, mock_get):
