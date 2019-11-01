a3b1ac842f93a39afdcbf9157ad5df8d591c2a7d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-01 18:02:30 +0000

    Add test/unit/elastic_class/get_cluster_status.py

---
+++ test/unit/elastic_class/get_cluster_status.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  get_cluster_status.py
+
+    Description:  Unit testing of get_cluster_status in elastic_class class.
+
+    Usage:
+        test/unit/elastic_class/get_cluster_status.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import elastic_class
+import version
+
+__version__ = version.__version__
+
+
+class Repo(object):
+
+    """Class:  Repo
+
+    Description:  Class representation of the cat class.
+
+    Methods:
+        health -> Stub holder for cluster.health method.
+
+    """
+
+    def health(self):
+
+        """Method:  health
+
+        Description:  Stub holder for cluster.health method.
+
+        Arguments:
+
+        """
+
+        return {"status": "green", "shards": "good", "disk": "good"}
+
+
+class Elasticsearch(object):
+
+    """Class:  ElasticSearch
+
+    Description:  Class representation of the Elasticsearch class.
+
+    Methods:
+        __init__ -> Initialize configuration environment.
+
+    """
+
+    def __init__(self, host_list, port=9200):
+
+        """Method:  __init__
+
+        Description:  Initialization instance of the class.
+
+        Arguments:
+
+        """
+
+        self.hosts = host_list
+        self.port = port
+        self.cluster = Repo()
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialization for unit testing.
+        test_default -> Test with default settings.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.host_list = ["host1", "host2"]
+        self.repo = "reponame"
+        self.es = Elasticsearch(self.host_list)
+        self.results = "green"
+
+    def test_default(self):
+
+        """Function:  test_default
+
+        Description:  Test with default settings.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(elastic_class.get_cluster_status(self.es),
+                         self.results)
+
+
+if __name__ == "__main__":
+    unittest.main()
