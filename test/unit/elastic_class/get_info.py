b249497e9586f635caa200f05f55ac60af0de468
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-01 16:12:07 +0000

    Add test/unit/elastic_class/get_info.py

---
+++ test/unit/elastic_class/get_info.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  get_info.py
+
+    Description:  Unit testing of get_info in elastic_class class.
+
+    Usage:
+        test/unit/elastic_class/get_info.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import elastic_class
+import version
+
+__version__ = version.__version__
+
+
+class Elasticsearch(object):
+
+    """Class:  ElasticSearch
+
+    Description:  Class representation of the Elasticsearch class.
+
+    Methods:
+        __init__ -> Initialize configuration environment.
+        info -> Stub holder for Elasticsearch.info method.
+
+    """
+
+    def __init__(self, host_list, port=9200):
+
+        """Method:  __init__
+
+        Description:  Initialization instance of the class.
+
+        Arguments:
+
+        """
+
+        self.hosts = host_list
+        self.port = port
+
+    def info(self):
+
+        """Method:  info
+
+        Description:  Stub holder for Elasticsearch.info method.
+
+        Arguments:
+
+        """
+
+        return {"cluster_name": "ClusterName", "name": "ServerName"}
+
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialization for unit testing.
+        test_default -> Test with default settings.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.host_list = ["host1", "host2"]
+        self.repo = "reponame"
+        self.es = Elasticsearch(self.host_list)
+        self.results = {"cluster_name": "ClusterName", "name": "ServerName"}
+
+    def test_default(self):
+
+        """Function:  test_default
+
+        Description:  Test with default settings.
+
+        Arguments:
+
+        """
+
+        self.assertEqual(elastic_class.get_info(self.es), self.results)
+
+
+if __name__ == "__main__":
+    unittest.main()
