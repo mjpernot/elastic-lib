20cbd930501434ce292e37940bb395ba00ee781c
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-05 17:53:26 +0000

    Update test/unit/elastic_class/ElasticSearchRepo_delete_repo.py

--- test/unit/elastic_class/ElasticSearchRepo_delete_repo.py
+++ test/unit/elastic_class/ElasticSearchRepo_delete_repo.py
@@ -178,9 +178,14 @@ class UnitTest(unittest.TestCase):
         self.repo3 = "reponame3"
         self.es = Elasticsearch(self.host_list)
         self.err_msg = "ERROR:  Repository still detected:  reponame"
+        self.nodes_data = {"serverid1": {"name": "hostname1", "settings":
+            {"path": {"data": ["/dir/data1"], "logs": ["/dir/logs1"]}}},
+            "serverid2": {"name": "hostname2", "settings":
+            {"path": {"data": ["/dir/data2"], "logs": ["/dir/logs2"]}}}}
 
+    @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_repo_name_failed(self, mock_es):
+    def test_repo_name_failed(self, mock_es, mock_nodes):
 
         """Function:  test_repo_name_failed
 
@@ -191,6 +196,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
+        mock_nodes.return_value = self.nodes_data
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo)
         es.repo_dict = {"reponame":
@@ -204,8 +210,9 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(es.delete_repo(self.repo3),
             (True, "ERROR:  Repository deletion failed:  reponame3"))
 
+    @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_repo_name_none(self, mock_es):
+    def test_repo_name_none(self, mock_es, mock_nodes):
 
         """Function:  test_repo_name_none
 
@@ -216,6 +223,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
+        mock_nodes.return_value = self.nodes_data
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo)
         es.repo_dict = {"reponame":
@@ -229,8 +237,9 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(es.delete_repo(),
             (True, "ERROR: Missing repo or does not exist: None"))
 
+    @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_no_repo_name(self, mock_es):
+    def test_no_repo_name(self, mock_es, mock_nodes):
 
         """Function:  test_no_repo_name
 
@@ -241,6 +250,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
+        mock_nodes.return_value = self.nodes_data
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo)
         es.repo_dict = {"reponame":
@@ -252,8 +262,9 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(es.delete_repo(self.repo2), (False, None))
 
+    @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_not_deleted(self, mock_es):
+    def test_not_deleted(self, mock_es, mock_nodes):
 
         """Function:  test_not_deleted
 
@@ -264,13 +275,15 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
+        mock_nodes.return_value = self.nodes_data
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo)
 
         self.assertEqual(es.delete_repo(self.repo), (True, self.err_msg))
 
+    @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_default(self, mock_es):
+    def test_default(self, mock_es, mock_nodes):
 
         """Function:  test_default
 
@@ -281,6 +294,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
+        mock_nodes.return_value = self.nodes_data
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo)
         es.repo_dict = {"reponame":
