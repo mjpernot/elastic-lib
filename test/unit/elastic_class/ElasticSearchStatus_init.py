c0b9ba2846114061eb7ea7c48545cf9f986e5829
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-06 16:34:34 +0000

    Add test/unit/elastic_class/ElasticSearchStatus_init.py

---
+++ test/unit/elastic_class/ElasticSearchStatus_init.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  ElasticStatus_init.py
+
+    Description:  Unit testing of __init__ in elastic_class.ElasticStatus
+        class.
+
+    Usage:
+        test/unit/elastic_class/ElasticStatus_init.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import elastic_class
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class Elasticsearch(object):
+
+    """Class:  ElasticSearch
+
+    Description:  Class representation of the Elasticsearch class.
+
+    Methods:
+        __init__ -> Initialize configuration environment.
+        ping -> Stub holder for Elasticsearch.ping method.
+        info -> Stub holder for Elasticsearch.info method.
+
+    """
+
+    def __init__(self, host_list, port=9200):
+
+        """Method:  __init__
+
+        Description:  Initialization instance of the class.
+
+        Arguments:
+
+        """
+
+        self.hosts = host_list
+        self.port = port
+        self.ping_status = True
+        self.info_status = {"cluster_name": "ClusterName", "name": "servername"}
+
+    def ping(self):
+
+        """Method:  ping
+
+        Description:  Stub holder for Elasticsearch.ping method.
+
+        Arguments:
+
+        """
+
+        return self.ping_status
+
+    def info(self):
+
+        """Method:  info
+
+        Description:  Stub holder for Elasticsearch.info method.
+
+        Arguments:
+
+        """
+
+        return self.info_status
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialization for unit testing.
+        test_disk_arg -> Test passing disk argument.
+        test_cpu_arg -> Test passing cpu argument.
+        test_mem_arg -> Test passing memory argument.
+        test_default -> Test with default settings.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.host_list = ["host1", "host2"]
+        self.es = Elasticsearch(self.host_list)
+
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+                mock.Mock(return_value=True))
+    def test_disk_arg(self):
+
+        """Function:  test_disk_arg
+
+        Description:  Test passing disk argument.
+
+        Arguments:
+
+        """
+
+        es = elastic_class.ElasticSearchStatus(self.host_list, cutoff_disk=30)
+        self.assertEqual((es.hosts, es.cutoff_disk), (self.host_list, 30))
+
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+                mock.Mock(return_value=True))
+    def test_cpu_arg(self):
+
+        """Function:  test_cpu_arg
+
+        Description:  Test passing cpu argument.
+
+        Arguments:
+
+        """
+
+
+        es = elastic_class.ElasticSearchStatus(self.host_list, cutoff_cpu=20)
+        self.assertEqual((es.hosts, es.cutoff_cpu), (self.host_list, 20))
+
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+                mock.Mock(return_value=True))
+    def test_mem_arg(self):
+
+        """Function:  test_mem_arg
+
+        Description:  Test passing memory argument.
+
+        Arguments:
+
+        """
+
+        es = elastic_class.ElasticSearchStatus(self.host_list, cutoff_mem=10)
+        self.assertEqual((es.hosts, es.cutoff_mem), (self.host_list, 10))
+
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+                mock.Mock(return_value=True))
+    def test_default(self):
+
+        """Function:  test_default
+
+        Description:  Test with default settings.
+
+        Arguments:
+
+        """
+
+        es = elastic_class.ElasticSearchStatus(self.host_list)
+        self.assertEqual((es.hosts, es.failed_nodes, es.alloc_cpu,
+                          es.cpu_active, es.pending_tasks),
+                         (self.host_list, None, None, None, None))
+
+
+if __name__ == "__main__":
+    unittest.main()
