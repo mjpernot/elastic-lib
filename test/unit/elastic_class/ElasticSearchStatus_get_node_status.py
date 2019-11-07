da8ebd348cac366086bde7d9ca21494e18b5e5f6
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-07 09:14:39 +0000

    Add new file ElasticSearchStatus_get_node_status.py

---
+++ test/unit/elastic_class/ElasticSearchStatus_get_node_status.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  ElasticSearchStatus_get_node_status.py
+
+    Description:  Unit testing of get_node_status in
+        elastic_class.ElasticSearchStatus.
+
+    Usage:
+        test/unit/elastic_class/ElasticSearchStatus_get_node_status.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import elastic_class
+import version
+
+__version__ = version.__version__
+
+
+class Elasticsearch(object):
+
+    """Class:  ElasticSearch
+
+    Description:  Class representation of the Elasticsearch class.
+
+    Methods:
+        __init__ -> Initialize configuration environment.
+
+    """
+
+    def __init__(self, host_list, port=9200):
+
+        """Method:  __init__
+
+        Description:  Initialization instance of the class.
+
+        Arguments:
+
+        """
+
+        self.hosts = host_list
+        self.port = port
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp -> Initialization for unit testing.
+        test_default -> Test with default settings.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.host_list = ["host1", "host2"]
+        self.es = Elasticsearch(self.host_list)
+        self.total_nodes = 3
+        self.failed_nodes = 0
+        self.results = {"NodeStatus": {"TotalNodes": 3, "FailedNodes": 0}}
+
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.elasticsearch.Elasticsearch")
+    def test_default(self, mock_es):
+
+        """Function:  test_default
+
+        Description:  Test with default settings.
+
+        Arguments:
+
+        """
+
+        mock_es.return_value = self.es
+
+        es = elastic_class.ElasticSearchStatus(self.host_list)
+        es.total_nodes = self.total_nodes
+        es.failed_nodes = self.failed_nodes
+
+        self.assertEqual(es.get_node_status(), self.results)
+
+
+if __name__ == "__main__":
+    unittest.main()
