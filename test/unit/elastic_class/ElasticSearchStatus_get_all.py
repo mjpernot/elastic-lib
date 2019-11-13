5c302243a244ae214edaaace0cf4849fe7239fd6
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-13 17:04:35 +0000

    Update test/unit/elastic_class/ElasticSearchStatus_get_all.py

--- test/unit/elastic_class/ElasticSearchStatus_get_all.py
+++ test/unit/elastic_class/ElasticSearchStatus_get_all.py
@@ -103,7 +103,7 @@ class UnitTest(unittest.TestCase):
                 mock.Mock(return_value={})) 
     @mock.patch("elastic_class.ElasticSearchStatus.get_disk_status",
                 mock.Mock(return_value={})) 
-    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status2",
                 mock.Mock(return_value=True))
     @mock.patch("elastic_class.ElasticSearch.update_status",
                 mock.Mock(return_value=True))
@@ -140,7 +140,7 @@ class UnitTest(unittest.TestCase):
                 mock.Mock(return_value={})) 
     @mock.patch("elastic_class.ElasticSearchStatus.get_disk_status",
                 mock.Mock(return_value={})) 
-    @mock.patch("elastic_class.ElasticSearchStatus.update_status",
+    @mock.patch("elastic_class.ElasticSearchStatus.update_status2",
                 mock.Mock(return_value=True))
     @mock.patch("elastic_class.ElasticSearch.update_status",
                 mock.Mock(return_value=True))
