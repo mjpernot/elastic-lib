66e3c17d37b3e53ac6dddc8af44382a216651630
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-14 12:23:30 +0000

    Update ElasticSearchRepo_update_repo_status.py

--- test/unit/elastic_class/ElasticSearchRepo_update_repo_status.py
+++ test/unit/elastic_class/ElasticSearchRepo_update_repo_status.py
@@ -36,58 +36,6 @@ import version
 __version__ = version.__version__
 
 
-class Repo2(object):
-
-    """Class:  Repo2
-
-    Description:  Class representation of the snapshot class.
-
-    Methods:
-        get_repository -> Stub holder for snapshot.get_repository method.
-
-    """
-
-    def get_repository(self):
-
-        """Method:  get_repository
-
-        Description:  Stub holder for snapshot.get_repository method.
-
-        Arguments:
-
-        """
-
-        return {"reponame": {"type": "dbdump", "settings":
-                             {"location": "/dir/path/dump"}},
-                "reponame2": {"type": "dbdump2", "settings":
-                             {"location": "/dir/path/dump2"}}}
-
-
-class Repo(object):
-
-    """Class:  Repo
-
-    Description:  Class representation of the snapshot class.
-
-    Methods:
-        get_repository -> Stub holder for snapshot.get_repository method.
-
-    """
-
-    def get_repository(self):
-
-        """Method:  get_repository
-
-        Description:  Stub holder for snapshot.get_repository method.
-
-        Arguments:
-
-        """
-
-        return {"reponame": {"type": "dbdump", "settings":
-                             {"location": "/dir/path/dump"}}}
-
-
 class Elasticsearch(object):
 
     """Class:  ElasticSearch
@@ -96,8 +44,6 @@ class Elasticsearch(object):
 
     Methods:
         __init__ -> Initialize configuration environment.
-        ping -> Stub holder for Elasticsearch.ping method.
-        info -> Stub holder for Elasticsearch.info method.
 
     """
 
@@ -116,31 +62,6 @@ class Elasticsearch(object):
         self.ping_status = True
         self.info_status = {"cluster_name": "ClusterName",
                             "name": "servername"}
-        self.snapshot = Repo()
-
-    def ping(self):
-
-        """Method:  ping
-
-        Description:  Stub holder for Elasticsearch.ping method.
-
-        Arguments:
-
-        """
-
-        return self.ping_status
-
-    def info(self):
-
-        """Method:  info
-
-        Description:  Stub holder for Elasticsearch.info method.
-
-        Arguments:
-
-        """
-
-        return self.info_status
 
 
 class UnitTest(unittest.TestCase):
@@ -171,29 +92,15 @@ class UnitTest(unittest.TestCase):
         self.repo = "reponame"
         self.repo2 = "reponame2"
         self.es = Elasticsearch(self.host_list)
-        self.dump_list = ["dump1", "dump2"]
-        self.nodes_data = {"serverid1": {"name": "hostname1", "settings":
-            {"path": {"data": ["/dir/data1"], "logs": ["/dir/logs1"]}}},
-            "serverid2": {"name": "hostname2", "settings":
-            {"path": {"data": ["/dir/data2"], "logs": ["/dir/logs2"]}}}}
-        self.health_data = {"status": "green", "cluster_name": "ClusterName"}
-        self.last_dump = "dump2"
+        self.repo_dict = {"reponame": {"type": "dbdump", "settings":
+            {"location": "/dir/path/dump"}}}
 
-    @mock.patch("elastic_class.get_cluster_nodes",
-                mock.Mock(return_value={"_nodes": {"total": 3}}))
-    @mock.patch("elastic_class.get_master_name",
-                mock.Mock(return_value="MasterName"))
-    @mock.patch("elastic_class.get_info",
-                mock.Mock(return_value={"name": "localservername"}))
-    @mock.patch("elastic_class.elastic_libs.get_latest_dump",
-                mock.Mock(return_value= "dump2"))
-    @mock.patch("elastic_class.is_active", mock.Mock(return_value=True))
-    @mock.patch("elastic_class.get_cluster_health")
-    @mock.patch("elastic_class.get_nodes")
-    @mock.patch("elastic_class.get_dump_list")
+
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
+    @mock.patch("elastic_class.is_active", mock.Mock(return_value=False))
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_repo_not_present(self, mock_es, mock_list, mock_nodes,
-                              mock_health):
+    def test_repo_not_present(self, mock_es):
 
         """Function:  test_repo_not_present
 
@@ -204,26 +111,17 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
-        mock_list.return_value = self.dump_list
-        mock_nodes.return_value = self.nodes_data
-        mock_health.return_value = self.health_data
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo2)
-        self.assertEqual((es.hosts, es.dump_list, es.repo_name,
-                          es.last_dump_name), (self.host_list, [], None, None))
+        self.assertEqual((es.hosts, es.repo_name, es.repo_dict),
+                         (self.host_list, None, {}))
 
-    @mock.patch("elastic_class.get_master_name",
-                mock.Mock(return_value="MasterName"))
-    @mock.patch("elastic_class.get_cluster_nodes",
-                mock.Mock(return_value={"_nodes": {"total": 3}}))
-    @mock.patch("elastic_class.get_info",
-                mock.Mock(return_value={"name": "localservername"}))
+    @mock.patch("elastic_class.ElasticSearch.update_status",
+                mock.Mock(return_value=True))
     @mock.patch("elastic_class.is_active", mock.Mock(return_value=True))
-    @mock.patch("elastic_class.get_cluster_health")
-    @mock.patch("elastic_class.get_nodes")
-    @mock.patch("elastic_class.get_dump_list")
+    @mock.patch("elastic_class.get_repo_list")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_default(self, mock_es, mock_list, mock_nodes, mock_health):
+    def test_default(self, mock_es, mock_repo):
 
         """Function:  test_default
 
@@ -234,13 +132,12 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_es.return_value = self.es
-        mock_list.return_value = self.dump_list
-        mock_nodes.return_value = self.nodes_data
-        mock_health.return_value = self.health_data
+        mock_repo.return_value = self.repo_dict
 
         es = elastic_class.ElasticSearchRepo(self.host_list, repo=self.repo)
-        self.assertEqual((es.hosts, es.dump_list, es.repo_name),
-                         (self.host_list, self.dump_list, self.repo))
+        self.assertEqual((es.hosts, es.dump_list, es.repo_name, es.repo_dict),
+                         (self.host_list, self.dump_list, self.repo,
+                          self.repo_dict))
 
 
 if __name__ == "__main__":
