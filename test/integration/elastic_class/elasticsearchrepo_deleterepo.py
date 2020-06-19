ce7ed2335cef87fb8d2d61ed3fd78217af5add63
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-06-19 09:21:21 +0100

    Update test/integration/elastic_class/elasticsearchrepo_deleterepo.py v2.0.2

--- test/integration/elastic_class/elasticsearchrepo_deleterepo.py
+++ test/integration/elastic_class/elasticsearchrepo_deleterepo.py
@@ -66,8 +66,8 @@ class UnitTest(unittest.TestCase):
         self.config_path = os.path.join(self.test_path, "config")
         self.cfg = gen_libs.load_module("elastic", self.config_path)
         self.repo_name = "TEST_REPO"
-        self.repo_dir = os.path.join(self.cfg.base_repo_dir, self.repo_name)
-
+        self.repo_dir = os.path.join(self.cfg.log_repo_dir, self.repo_name)
+        self.phy_repo_dir = os.path.join(self.cfg.phy_repo_dir, self.repo_name)
         esr = elastic_class.ElasticSearchRepo(self.cfg.host,
                                               repo=self.repo_name)
 
@@ -141,8 +141,8 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        if os.path.isdir(self.repo_dir):
-            shutil.rmtree(self.repo_dir)
+        if os.path.isdir(self.phy_repo_dir):
+            shutil.rmtree(self.phy_repo_dir)
 
 
 if __name__ == "__main__":
