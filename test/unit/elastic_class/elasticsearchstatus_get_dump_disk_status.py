cda3aedf24d211f281f99b5584b7daf28b518aca
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-10-18 14:35:20 +0100

    Update test/unit/elastic_class/elasticsearchstatus_get_dump_disk_status.py v3.0.0

--- test/unit/elastic_class/elasticsearchstatus_get_dump_disk_status.py
+++ test/unit/elastic_class/elasticsearchstatus_get_dump_disk_status.py
@@ -43,7 +43,7 @@ class Elasticsearch(object):
     Description:  Class representation of the Elasticsearch class.
 
     Methods:
-        __init__ -> Initialize configuration environment.
+        __init__
 
     """
 
@@ -68,8 +68,8 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialization for unit testing.
-        test_default -> Test with default settings.
+        setUp
+        test_default
 
     """
 
