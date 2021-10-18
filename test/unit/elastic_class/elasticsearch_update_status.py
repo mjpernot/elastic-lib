61d775038dbb53ce642bd86aa52f3e470c2f7b30
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-10-18 14:44:40 +0100

    Update test/unit/elastic_class/elasticsearch_update_status.py v3.0.0

--- test/unit/elastic_class/elasticsearch_update_status.py
+++ test/unit/elastic_class/elasticsearch_update_status.py
@@ -41,8 +41,8 @@ class Elasticsearch(object):
     Description:  Class representation of the Elasticsearch class.
 
     Methods:
-        __init__ -> Initialize configuration environment.
-        info -> Stub holder for Elasticsearch.info method.
+        __init__
+        info
 
     """
 
@@ -82,9 +82,9 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialization for unit testing.
-        test_ping_false -> Test ping of Elasticsearch server is False.
-        test_ping_true -> Test ping of Elasticsearch server is True.
+        setUp
+        test_ping_false
+        test_ping_true
 
     """
 
