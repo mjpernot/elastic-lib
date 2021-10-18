be255e01f59182dd4b05cfa1abe031c8ddae1b2e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-10-18 14:44:54 +0100

    Update test/unit/elastic_class/elasticsearchdump_init.py v3.0.0

--- test/unit/elastic_class/elasticsearchdump_init.py
+++ test/unit/elastic_class/elasticsearchdump_init.py
@@ -41,8 +41,8 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialization for unit testing.
-        test_default -> Test with default settings.
+        setUp
+        test_default
 
     """
 
