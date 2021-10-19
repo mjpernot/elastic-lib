28333205326db0f76be0b9f2a33c055af9848041
Mark Pernot <mark.pernot@gmail.com>
2021-10-19 11:21:21 +0100

    Update test/unit/elastic_class/elasticsearch_update_status.py v3.0.0

--- test/unit/elastic_class/elasticsearch_update_status.py
+++ test/unit/elastic_class/elasticsearch_update_status.py
@@ -83,8 +83,9 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp
-        test_ping_false
-        test_ping_true
+        test_single_node
+        test_multiple_nodes
+        test_update_status
 
     """
 
@@ -107,6 +108,9 @@ class UnitTest(unittest.TestCase):
                                          {"path":
                                           {"data": ["/dir/data2"],
                                            "logs": ["/dir/logs2"]}}}}
+        self.nodes_data2 = {"serverid1": {"name": "hostname1", "settings":
+                                          {"path": {"data": ["/dir/data1"],
+                                                    "logs": ["/dir/logs1"]}}}}
         self.info_data = {"name": "localservername"}
         self.health_data = {"status": "green", "cluster_name": "ClusterName"}
         self.master_name = "MasterName"
@@ -116,26 +120,34 @@ class UnitTest(unittest.TestCase):
         self.logs_results = {"hostname1": ["/dir/logs1"],
                              "hostname2": ["/dir/logs2"]}
 
-    @mock.patch("elastic_class.is_active", mock.Mock(return_value=False))
+    @mock.patch("elastic_class.is_active", mock.Mock(return_value=True))
+    @mock.patch("elastic_class.get_cluster_nodes")
+    @mock.patch("elastic_class.get_master_name")
+    @mock.patch("elastic_class.get_cluster_health")
+    @mock.patch("elastic_class.get_info")
+    @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_ping_false(self, mock_es):
+    def test_single_node(self, mock_es, mock_nodes, mock_info, mock_health,
+                         mock_master, mock_cluster):
 
-        """Function:  test_ping_false
+        """Function:  test_single_node
 
-        Description:  Test ping of Elasticsearch server is False.
+        Description:  Test with one node.
 
         Arguments:
 
         """
 
-        self.els.ping_status = False
         mock_es.return_value = self.els
+        mock_nodes.return_value = self.nodes_data2
+        mock_info.return_value = self.info_data
+        mock_health.return_value = self.health_data
+        mock_master.return_value = self.master_name
+        mock_cluster.return_value = self.cluster_data
 
         els = elastic_class.ElasticSearch(self.host_list)
-        els.update_status()
-        self.assertEqual((els.port, els.hosts, els.is_connected, els.data,
-                          els.logs),
-                         (9200, self.host_list, False, {}, {}))
+        els.connect()
+        self.assertEqual(len(els.nodes), 1)
 
     @mock.patch("elastic_class.is_active", mock.Mock(return_value=True))
     @mock.patch("elastic_class.get_cluster_nodes")
@@ -144,12 +156,12 @@ class UnitTest(unittest.TestCase):
     @mock.patch("elastic_class.get_info")
     @mock.patch("elastic_class.get_nodes")
     @mock.patch("elastic_class.elasticsearch.Elasticsearch")
-    def test_ping_true(self, mock_es, mock_nodes, mock_info, mock_health,
-                       mock_master, mock_cluster):
+    def test_multiple_nodes(self, mock_es, mock_nodes, mock_info, mock_health,
+                            mock_master, mock_cluster):
 
-        """Function:  test_ping_true
+        """Function:  test_multiple_nodes
 
-        Description:  Test ping of Elasticsearch server is True.
+        Description:  Test with two nodes.
 
         Arguments:
 
@@ -163,11 +175,39 @@ class UnitTest(unittest.TestCase):
         mock_cluster.return_value = self.cluster_data
 
         els = elastic_class.ElasticSearch(self.host_list)
-        els.update_status()
-        self.assertEqual((els.port, els.hosts, els.is_connected, els.data,
-                          els.logs),
-                         (9200, self.host_list, True, self.data_results,
-                          self.logs_results))
+        els.connect()
+        self.assertEqual(len(els.nodes), 2)
+
+    @mock.patch("elastic_class.is_active", mock.Mock(return_value=True))
+    @mock.patch("elastic_class.get_cluster_nodes")
+    @mock.patch("elastic_class.get_master_name")
+    @mock.patch("elastic_class.get_cluster_health")
+    @mock.patch("elastic_class.get_info")
+    @mock.patch("elastic_class.get_nodes")
+    @mock.patch("elastic_class.elasticsearch.Elasticsearch")
+    def test_update_status(self, mock_es, mock_nodes, mock_info, mock_health,
+                           mock_master, mock_cluster):
+
+        """Function:  test_update_status
+
+        Description:  Test with update_status method.
+
+        Arguments:
+
+        """
+
+        mock_es.return_value = self.els
+        mock_nodes.return_value = self.nodes_data
+        mock_info.return_value = self.info_data
+        mock_health.return_value = self.health_data
+        mock_master.return_value = self.master_name
+        mock_cluster.return_value = self.cluster_data
+
+        els = elastic_class.ElasticSearch(self.host_list)
+        els.connect()
+        self.assertEqual(
+            (els.port, els.hosts, els.is_connected, els.data, els.logs),
+            (9200, self.host_list, True, self.data_results, self.logs_results))
 
 
 if __name__ == "__main__":
