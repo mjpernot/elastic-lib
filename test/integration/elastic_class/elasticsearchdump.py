e85f274884a33cb2941e23246cc69dcf466358ec
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-11-03 16:32:44 +0000

    Update test/integration/elastic_class/elasticsearchdump.py v3.0.0

--- test/integration/elastic_class/elasticsearchdump.py
+++ test/integration/elastic_class/elasticsearchdump.py
@@ -43,17 +43,11 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp
-        test_dump_list_is_set
         test_dump_list_is_empty
-        test_repo_is_set
         test_repo_not_set
-        test_multi_repo
-        test_single_repo
         test_repo_not_passed
         test_repo_not_exist
-        test_connect
         test_init
-        tearDown
 
     """
 
@@ -72,41 +66,6 @@ class UnitTest(unittest.TestCase):
         self.config_path = os.path.join(self.test_path, "config")
         self.cfg = gen_libs.load_module("elastic", self.config_path)
         self.repo_name = "TEST_INTR_REPO"
-        self.repo_name2 = "TEST_INTR_REPO2"
-        self.repo_dir = os.path.join(self.cfg.log_repo_dir, self.repo_name)
-        self.repo_dir2 = os.path.join(self.cfg.log_repo_dir, self.repo_name2)
-        self.phy_repo_dir = os.path.join(self.cfg.phy_repo_dir, self.repo_name)
-        self.phy_repo_dir2 = os.path.join(self.cfg.phy_repo_dir,
-                                          self.repo_name2)
-        esr = elastic_class.ElasticSearchRepo(
-            self.cfg.host, repo=self.repo_name, repo_dir=self.repo_dir)
-
-        if esr.repo_dict:
-            print("ERROR: Test environment not clean - repositories exist.")
-            self.skipTest("Pre-conditions not met.")
-
-    def test_dump_list_is_set(self):
-
-        """Function:  test_dump_list_is_set
-
-        Description:  Test if dump list has data in it.
-
-        Arguments:
-
-        """
-
-        esr = elastic_class.ElasticSearchRepo(
-            self.cfg.host, repo=self.repo_name, repo_dir=self.repo_dir)
-        esr.create_repo()
-        esd = elastic_class.ElasticSearchDump(self.cfg.host,
-                                              repo=self.repo_name)
-        esd.dump_db()
-        esd2 = elastic_class.ElasticSearchDump(self.cfg.host,
-                                               repo=self.repo_name)
-
-        self.assertTrue(esd2.dump_list)
-
-        esr.delete_repo()
 
     def test_dump_list_is_empty(self):
 
@@ -118,33 +77,9 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        esr = elastic_class.ElasticSearchRepo(
-            self.cfg.host, repo=self.repo_name, repo_dir=self.repo_dir)
-        esr.create_repo()
         esd = elastic_class.ElasticSearchDump(self.cfg.host)
 
         self.assertTrue(not esd.dump_list)
-
-        esr.delete_repo()
-
-    def test_repo_is_set(self):
-
-        """Function:  test_repo_is_set
-
-        Description:  Test if dump location is set.
-
-        Arguments:
-
-        """
-
-        esr = elastic_class.ElasticSearchRepo(
-            self.cfg.host, repo=self.repo_name, repo_dir=self.repo_dir)
-        esr.create_repo()
-        esd = elastic_class.ElasticSearchDump(self.cfg.host)
-
-        self.assertTrue(esd.dump_loc == self.repo_dir)
-
-        esr.delete_repo()
 
     def test_repo_not_set(self):
 
@@ -160,46 +95,6 @@ class UnitTest(unittest.TestCase):
 
         self.assertTrue(not esd.type)
 
-    def test_multi_repo(self):
-
-        """Function:  test_multi_repo
-
-        Description:  Test if multiple repos are present.
-
-        Arguments:
-
-        """
-
-        esr = elastic_class.ElasticSearchRepo(
-            self.cfg.host, repo=self.repo_name, repo_dir=self.repo_dir)
-        esr.create_repo()
-        esr.create_repo(repo_name=self.repo_name2, repo_dir=self.repo_dir2)
-        esd = elastic_class.ElasticSearchDump(self.cfg.host)
-
-        self.assertTrue(esd.repo_name is None)
-
-        esr.delete_repo()
-        esr.delete_repo(repo_name=self.repo_name2)
-
-    def test_single_repo(self):
-
-        """Function:  test_single_repo
-
-        Description:  Test if single repo is present.
-
-        Arguments:
-
-        """
-
-        esr = elastic_class.ElasticSearchRepo(
-            self.cfg.host, repo=self.repo_name, repo_dir=self.repo_dir)
-        esr.create_repo()
-        esd = elastic_class.ElasticSearchDump(self.cfg.host)
-
-        self.assertTrue(esd.repo_name == self.repo_name)
-
-        esr.delete_repo()
-
     def test_repo_not_passed(self):
 
         """Function:  test_repo_not_passed
@@ -214,34 +109,20 @@ class UnitTest(unittest.TestCase):
 
         self.assertTrue(not esd.repo_name and not esd.type)
 
-    def test_repo_not_exist(self):
+    def test_repo_passed(self):
 
-        """Function:  test_repo_not_exist
+        """Function:  test_repo_passed
 
-        Description:  Test if repo name does not exist.
-
-        Arguments:
-
-        """
-
-        esd = elastic_class.ElasticSearchDump(self.cfg.host,
-                                              repo=self.repo_name)
-
-        self.assertTrue(not esd.repo_name and not esd.type)
-
-    def test_connect(self):
-
-        """Function:  test_connect
-
-        Description:  Test to successfully connect to Elasticsearch.
+        Description:  Test if repo name is passed.
 
         Arguments:
 
         """
 
-        esd = elastic_class.ElasticSearchDump(self.cfg.host)
+        esd = elastic_class.ElasticSearchDump(
+            self.cfg.host, repo=self.repo_name)
 
-        self.assertTrue(not esd.repo_name and not esd.type)
+        self.assertEqual(esd.repo_name,  self.repo_name)
 
     def test_init(self):
 
@@ -253,28 +134,9 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        host_list = "Host_Name"
-
-        with gen_libs.no_std_out():
-            esd = elastic_class.ElasticSearchDump(host_list)
+        esd = elastic_class.ElasticSearchDump(self.cfg.host)
 
         self.assertTrue(not esd.dump_name)
-
-    def tearDown(self):
-
-        """Function:  tearDown
-
-        Description:  Clean up of integration testing.
-
-        Arguments:
-
-        """
-
-        if os.path.isdir(self.phy_repo_dir):
-            shutil.rmtree(self.phy_repo_dir)
-
-        if os.path.isdir(self.phy_repo_dir2):
-            shutil.rmtree(self.phy_repo_dir2)
 
 
 if __name__ == "__main__":
