697511f96c3721a96944d0765f4402f28ad71954
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-11-05 16:19:28 +0000

    Add test/integration/elastic_class/elasticsearchrepo_connect.py v3.0.0

---
+++ test/integration/elastic_class/elasticsearchrepo_connect.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  elasticsearchrepo_connect.py
+
+    Description:  Integration testing of connect method in ElasticSearchRepo
+        class in elastic_class.py.
+
+    Usage:
+        test/integration/elastic_class/elasticsearchrepo_connect.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import lib.gen_libs as gen_libs
+import elastic_class
+import version
+
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp
+        test_connect
+        test_init
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.base_dir = "test/integration/elastic_class"
+        self.test_path = os.path.join(os.getcwd(), self.base_dir)
+        self.config_path = os.path.join(self.test_path, "config")
+        self.cfg = gen_libs.load_module("elastic", self.config_path)
+        self.repo_name = "TEST_INTR_REPO"
+        self.repo_dir = os.path.join(self.cfg.log_repo_dir, self.repo_name)
+        esr = elastic_class.ElasticSearchRepo(
+            self.cfg.host, repo=self.repo_name)
+
+        if esr.repo_dict:
+            print("ERROR: Test environment not clean - repositories exist.")
+            self.skipTest("Pre-conditions not met.")
+
+    def test_connect(self):
+
+        """Function:  test_connect
+
+        Description:  Test to successfully connect to Elasticsearch.
+
+        Arguments:
+
+        """
+
+        esr = elastic_class.ElasticSearchRepo(
+            self.cfg.host, user=self.cfg.user, japd=self.cfg.japd)
+        esr.connect()
+
+        self.assertTrue(not esr.repo and not esr.repo_dict)
+
+    def test_init(self):
+
+        """Function:  test_init
+
+        Description:  Test to see if class instance is created.
+
+        Arguments:
+
+        """
+
+        esr = elastic_class.ElasticSearchRepo(
+            self.cfg.host, user=self.cfg.user, japd=self.cfg.japd)
+        esr.connect()
+
+        self.assertTrue(not esr.repo)
+
+
+if __name__ == "__main__":
+    unittest.main()
