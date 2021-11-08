00d27a496d859583a19bfd1100bb26a5dd6c901b
Mark Pernot <mark.pernot@gmail.com>
2021-11-08 11:34:09 +0000

    Add test/integration/elastic_class/elasticsearchstatus_get_mem_status.py v3.0.0

---
+++ test/integration/elastic_class/elasticsearchstatus_get_mem_status.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  elasticsearchstatus_get_mem_status.py
+
+    Description:  Integration testing of get_mem_status method in
+        ElasticSearchStatus class in elastic_class.py.
+
+    Usage:
+        test/integration/elastic_class/elasticsearchstatus_get_mem_status.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import lib.gen_libs as gen_libs
+import elastic_class
+import version
+
+__version__ = version.__version__
+
+
+def bytes_2_readable(size, precision=2):
+
+    """Function:  bytes_2_readable
+
+    Description:  Converts a size (in bytes) in the appropriate readable size
+        with post-tag symbol.
+
+    Arguments:
+        (input) size -> Size in bytes to convert.
+        (input) precision -> Percision after decimal.
+
+    """
+
+    suffix = ["B", "KB", "MB", "GB", "TB"]
+    suf_index = 0
+
+    while size > 1024 and suf_index < 4:
+        suf_index += 1
+        size = size / 1024.0
+
+    return "%.*f%s" % (precision, size, suffix[suf_index])
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Methods:
+        setUp
+        test_data
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.base_dir = "test/integration/elastic_class"
+        self.test_path = os.path.join(os.getcwd(), self.base_dir)
+        self.config_path = os.path.join(self.test_path, "config")
+        self.cfg = gen_libs.load_module("elastic", self.config_path)
+
+    def test_data(self):
+
+        """Function:  test_data
+
+        Description:  Test with correct data is returned.
+
+        Arguments:
+
+        """
+
+        ess = elastic_class.ElasticSearchStatus(
+            self.cfg.host, user=self.cfg.user, japd=self.cfg.japd)
+        ess.connect()
+        mem_total = bytes_2_readable(ess.mem_total)
+        mem_used = bytes_2_readable(ess.mem_used)
+        mem_free = bytes_2_readable(ess.mem_free)
+        results = {
+            "Memory": {"Percent": ess.mem_per_used, "Total": mem_total,
+                       "Used": mem_used, "Free": mem_free}}
+
+        self.assertEqual(ess.get_mem_status(), results)
+
+
+if __name__ == "__main__":
+    unittest.main()
