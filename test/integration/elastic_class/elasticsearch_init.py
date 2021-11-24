05c4b331fd9cc2a3753daa37aca00dd77042d68e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-11-24 16:26:14 +0000

    Update test/integration/elastic_class/elasticsearch_init.py v3.0.1

--- test/integration/elastic_class/elasticsearch_init.py
+++ test/integration/elastic_class/elasticsearch_init.py
@@ -43,6 +43,8 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp
+        test_passed_scheme
+        test_default_scheme
         test_ca_cert_passed
         test_no_ca_cert_passed
         test_login_info_passed
@@ -64,10 +66,49 @@ class UnitTest(unittest.TestCase):
 
         """
 
+        self.scheme = "http"
         self.base_dir = "test/integration/elastic_class"
         self.test_path = os.path.join(os.getcwd(), self.base_dir)
         self.config_path = os.path.join(self.test_path, "config")
         self.cfg = gen_libs.load_module("elastic", self.config_path)
+
+    def test_passed_scheme(self):
+
+        """Function:  test_passed_scheme
+
+        Description:  Test with scheme passed in.
+
+        Arguments:
+
+        """
+
+        temp_val = self.cfg.ssl_client_ca
+        self.cfg.ssl_client_ca = "ca_cert.pem"
+        els = elastic_class.ElasticSearch(
+            self.cfg.host, user=self.cfg.user, japd=self.cfg.japd,
+            ca_cert=self.cfg.ssl_client_ca, scheme=self.scheme)
+        self.cfg.ssl_client_ca = temp_val
+
+        self.assertEqual(els.config["scheme"], "http")
+
+    def test_default_scheme(self):
+
+        """Function:  test_default_scheme
+
+        Description:  Test with default scheme used.
+
+        Arguments:
+
+        """
+
+        temp_val = self.cfg.ssl_client_ca
+        self.cfg.ssl_client_ca = "ca_cert.pem"
+        els = elastic_class.ElasticSearch(
+            self.cfg.host, user=self.cfg.user, japd=self.cfg.japd,
+            ca_cert=self.cfg.ssl_client_ca)
+        self.cfg.ssl_client_ca = temp_val
+
+        self.assertEqual(els.config["scheme"], "https")
 
     def test_ca_cert_passed(self):
 
