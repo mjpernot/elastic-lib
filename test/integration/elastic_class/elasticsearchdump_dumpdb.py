ba1ef44c40bebcb112b1b297d57b85f74081e82c
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-03-14 16:46:02 +0000

    Merge of mod/400c1 to mod/400

--- test/integration/elastic_class/elasticsearchdump_dumpdb.py
+++ test/integration/elastic_class/elasticsearchdump_dumpdb.py
@@ -273,7 +273,11 @@ class UnitTest(unittest.TestCase):
 
         err_flag, status_msg = esd.dump_db()
 
-        self.assertEqual((err_flag, status_msg), (True, self.msg))
+        if len(elastic_class(esd).keys()) == 0:
+            self.assertEqual((err_flag, status_msg), (True, self.msg))
+
+        else:
+            self.assertEqual((err_flag, status_msg), (False, None))
 
     def tearDown(self):
 
